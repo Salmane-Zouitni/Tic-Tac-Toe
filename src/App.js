67f151d697619f3f26e18b581b672e0ee95765a2
Salmane-Zouitni <salmane.zouitni@gmail.com>
2022-04-08 02:14:47 +0000

    lifting state up from Board Component to the Game Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Board from "./components/Board";
+import Game from "./components/Game";
 // import Square from "./components/Square";
 function App() {
   return (
     <div>
-      <Board />
+      <Game />
     </div>
   );
 }
