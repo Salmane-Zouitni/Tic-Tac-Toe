650fd276f67d520aff1278b51d030bcd44ef0977
Salmane-Zouitni <salmane.zouitni@gmail.com>
2022-04-11 02:59:05 +0000

    adding moves history display to the game

--- src/components/Game.js
+++ src/components/Game.js
@@ -32,7 +32,18 @@ class Game extends React.Component {
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
-  
+
+      const moves = history.map((step, move) => {
+        const desc = move ? 
+        'Go To Move ' + move:
+        'Go To Game Start';
+        return (
+        <li>
+          <button onClick={() => this.jumpTo(move)} >{desc}</button>
+        </li>
+        )
+      })
+
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
@@ -50,7 +61,7 @@ class Game extends React.Component {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
